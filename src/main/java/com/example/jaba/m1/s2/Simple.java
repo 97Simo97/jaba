9562cd7e8e6c075c82dd5a6bc44cc67a8aaabde1
Simone Ritucci <serc@gft.com>
2023-10-05 11:35:50 +0200

    update

--- src/main/java/com/example/jaba/m1/s2/Simple.java
+++ src/main/java/com/example/jaba/m1/s2/Simple.java
@@ -21,7 +21,7 @@ public class Simple {
      */
     public static void main(String[] args) {
         // output a string to console
-        System.out.print("Hello Simo!");
+        System.out.print("Hello ");
 
         // output is based on input arguments
         if (args.length == 1) {
